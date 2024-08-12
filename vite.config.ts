b793ce8ecaedacf967c9137120c50df8082ddfa1
Felipe Biel <felipe.biel@t10.digital>
2024-08-12 15:55:21 -0300

    feat: Adicionando o alias na configuração do Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/vue-knowledge/',
   plugins: [vue()],
+  // setando a porta padrão
+  server: {
+      host: true,
+      port: 8080, // <-- Set your own server port here
+  },
+  resolve: {
+      alias: [
+          {
+              find: '@',
+              replacement: path.resolve(__dirname, './src'),
+          },
+      ],
+  },
 })
