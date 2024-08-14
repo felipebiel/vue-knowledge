7e01ef0daca94d56af77130cc33cbd11e17625bf
Felipe Biel <felipe.biel@t10.digital>
2024-08-14 15:53:19 -0300

    feat: Configurando eslint e formatando código existente

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,20 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue-knowledge/',
-  plugins: [vue()],
-  // setando a porta padrão
-  server: {
-      host: true,
-      port: 8080, // <-- Set your own server port here
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+    base: '/vue-knowledge/',
+    plugins: [vue()],
+    // setando a porta padrão
+    server: {
+        host: true,
+        port: 8080, // <-- Set your own server port here
+    },
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
+    },
+});
