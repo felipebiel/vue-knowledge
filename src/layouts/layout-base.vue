a06a654f3df1e549a484bed8e0c25b4b29e82f01
Felipe Biel <felipe.biel@t10.digital>
2024-08-15 15:07:10 -0300

    feat: Arquivo base de layout

---
+++ src/layouts/layout-base.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div class="w-full relative">
+            <!-- <side-menu></side-menu> -->
+        </div>
+        <div class="main-box" :class="{}">
+            <!-- <toolbar-component></toolbar-component> -->
+            <div class="content-box p-5">
+                <router-view></router-view>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.main-box {
+    @apply absolute w-full left-0 lg:w-[calc(100%-300px)] lg:left-[300px] min-h-screen duration-500 bg-zinc-700;
+}
+</style>
