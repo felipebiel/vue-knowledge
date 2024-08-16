604d7761eadaf8ae11868e028b8dcc7e483fe2d1
Felipe Biel <felipe.biel@t10.digital>
2024-08-16 16:59:55 -0300

    feat: Botão de toggle do darkmode

--- src/layouts/toolbar/toolbar-component.vue
+++ src/layouts/toolbar/toolbar-component.vue
@@ -1,16 +1,21 @@
 <template>
     <div class="toolbar">
         <button class="toolbar__toggle" @click="toogleFunction"><span class="material-icons"> menu </span></button>
-        <div class="user">
+        <div class="toolbar__functions">
+            <button class="darkmode__button" @click="darkMode.toogleDarkMode">
+                <span class="material-icons darkmode__icon"> {{ darkMode.isDarkMode ? 'light_mode' : 'dark_mode' }}</span>
+            </button>
             <span class="material-icons user__icon"> account_circle </span>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
+import { darkModeStore } from '@/stores/dark-mode';
 import { sideMenuStore } from '@/stores/side-menu';
 
 const { toogleFunction } = sideMenuStore();
+const darkMode = darkModeStore();
 </script>
 
 <style lang="scss" scoped>
@@ -19,8 +24,17 @@ const { toogleFunction } = sideMenuStore();
     .toolbar__toggle {
         @apply relative w-12 h-12 flex justify-center items-center text-4xl cursor-pointer hover:bg-zinc-100 rounded-full;
     }
-    .user {
-        @apply relative;
+    .toolbar__functions {
+        @apply relative flex items-center gap-4;
+
+        .darkmode {
+            &__button {
+                @apply w-12 h-12 text-primary hover:bg-zinc-100 rounded-full;
+            }
+            &__icon {
+                @apply text-3xl;
+            }
+        }
 
         .user__icon {
             @apply text-[2.5rem] text-primary flex;
