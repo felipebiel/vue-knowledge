2286e8aa26b0a6a1379171cad73205e47b90072c
Felipe Biel <felipe.biel@t10.digital>
2024-08-19 10:25:37 -0300

    feat: Exemplo na home da aplicação

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,9 +1,16 @@
 <template>
-    <div>
-        <fb-card-info numbers="2,352" title="Visualizações" icon="visibility" :truncateSize="7"></fb-card-info>
+    <div class="home">
+        <div class="relative w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-8">
+            <fb-card-info numbers="2,352" title="Visualizações" icon="visibility" :truncateSize="5"></fb-card-info>
+            <fb-card-info numbers="80" title="Vendas" icon="shopping_cart" :truncateSize="5"></fb-card-info>
+            <fb-card-info numbers="1,381" title="Comentários" icon="question_answer" :truncateSize="5"></fb-card-info>
+            <fb-card-info numbers="$80548,36" title="Renda" icon="paid" :truncateSize="5"></fb-card-info>
+        </div>
     </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import FbCardInfo from '@/components/ui/fb-card-info.vue';
+</script>
 
 <style scoped></style>
