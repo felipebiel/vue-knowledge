4a3f18d229f88b5082fdd0e550f563bdafb1bddd
Felipe Biel <felipe.biel@t10.digital>
2024-08-19 11:51:32 -0300

    feat: Componente base de card + doc storybook

---
+++ src/components/ui/fb-card.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="fb-card">
+        <slot></slot>
+    </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.fb-card {
+    @apply p-5 sm:px-10 sm:py-11 shadow-app rounded-3xl bg-white dark:bg-zinc-600;
+}
+</style>
