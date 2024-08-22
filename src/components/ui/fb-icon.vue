339354be9d0349acc345e6a23c404c2e5ea84075
Felipe Biel <felipe.biel@t10.digital>
2024-08-22 15:59:44 -0300

    feat: Componente base de ícones customizados

---
+++ src/components/ui/fb-icon.vue
@@ -0,0 +1,26 @@
+<template>
+    <svg :color="color" :width="size" :height="size" v-html="nameIcon" :viewBox="viewBoxIcon" role="presentation" class="fb-icon"></svg>
+</template>
+
+<script setup lang="ts">
+import customIcons from '@/theme/custom-icons';
+import { computed } from 'vue';
+
+export interface IconProps {
+    size?: string;
+    name?: string;
+    viewBox?: string;
+    color?: string;
+}
+const props = withDefaults(defineProps<IconProps>(), { name: 'info', size: '16px', color: 'red' });
+
+const viewBoxIcon = computed(() => {
+    return customIcons[props.name] && customIcons[props.name].viewBox ? customIcons[props.name].viewBox : props.viewBox;
+});
+
+const nameIcon = computed(() => {
+    return customIcons[props.name] && customIcons[props.name].path ? customIcons[props.name].path : 'info';
+});
+</script>
+
+<style scoped></style>
