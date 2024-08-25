d5fb44cfb59ba2fe229e06549eb5817e8af45ec6
Felipe Biel <felipe.biel@t10.digital>
2024-08-25 14:12:50 -0300

    doc: Documentando a tabela

--- src/components/ui/fb-table.vue
+++ src/components/ui/fb-table.vue
@@ -1,11 +1,11 @@
 <template>
     <div></div>
     <div class="table-responsive">
-        <table id="fb-table">
+        <table class="fb-table">
             <thead>
-                <tr class="tr-head">
+                <tr class="fb-table__tr-head">
                     <th
-                        class="title"
+                        class="fb-table__title"
                         :key="index"
                         v-for="(header, index) in headers"
                         :style="{ width: header.columnSize ? header.columnSize : 'auto' }"
@@ -17,14 +17,14 @@
             <tbody>
                 <template v-if="rows.length > 0">
                     <tr :key="index" v-for="(row, index) in rows" @click="$emit('selectedRow', row)">
-                        <td v-for="(col, index) in columns" :key="index" class="table-value">
+                        <td v-for="(col, index) in columns" :key="index" class="fb-table__table-value">
                             <slot v-bind:item="row" :name="col">{{ row[col] }}</slot>
                         </td>
                     </tr>
                 </template>
                 <template v-else>
                     <tr>
-                        <td :colspan="columns.length" class="no-register">
+                        <td :colspan="columns.length" class="fb-table__no-register">
                             <slot name="no-register">Nenhum registro foi encontrado</slot>
                         </td>
                     </tr>
@@ -37,12 +37,20 @@
 <script setup lang="ts">
 import { computed } from 'vue';
 
+export interface IHeaders {
+    label: string;
+    value: string;
+    columnSize?: string;
+}
+
 export interface TableProps {
-    headers: Array<any>;
+    headers: Array<IHeaders>;
     items?: Array<any>;
 }
 
 const props = withDefaults(defineProps<TableProps>(), {});
+
+defineEmits(['selectedRow']);
 
 const rows = computed(() => {
     return props.items ? props.items : [];
@@ -59,31 +67,26 @@ table {
     @apply border-collapse w-full;
 }
 
-#fb-table tbody {
-    @apply align-top;
-}
-.tr-head {
-    @apply text-primary;
-}
-.tr-head .title {
-    @apply text-sm font-bold text-left p-6;
-    /* font-size: 13px;
-    font-weight: bold;
-    text-align: left;
-    padding: 1rem /* 24px */
-}
-.table-value {
-    @apply border-t border-t-zinc-200 text-sm text-secundary font-semibold p-6;
-}
-
-.no-register {
-    @apply border-t border-t-zinc-200 text-sm text-center text-zinc-400 font-semibold p-6;
-}
-
 .table-responsive {
     @apply overflow-x-scroll sm:overflow-x-auto;
-    /* @media screen and (max-width: 767px) {
-        overflow-x: scroll;
-    } */
+}
+
+.fb-table {
+    tbody {
+        @apply align-top;
+    }
+    &__tr-head {
+        @apply text-primary;
+    }
+    &__title {
+        @apply text-sm font-bold text-left p-6;
+    }
+    &__table-value {
+        @apply border-t border-t-zinc-200 text-sm text-secundary font-semibold p-6;
+    }
+
+    &__no-register {
+        @apply border-t border-t-zinc-200 text-sm text-center text-zinc-400 font-semibold p-6;
+    }
 }
 </style>
--- src/views/component-examples.vue
+++ src/views/component-examples.vue
@@ -1,47 +1,112 @@
 <template>
-    <div class="component-examples">
-        <fb-title-bar class="mb-4">
-            <template v-slot:left>Botões</template>
-        </fb-title-bar>
-        <fb-title-bar class="mb-4" title-type="subtitle">
-            <template v-slot:left>Variantes</template>
-        </fb-title-bar>
-        <div class="flex gap-2">
-            <fb-button>Meu botão</fb-button>
-            <fb-button variant="primary-outline">Meu botão</fb-button>
-            <fb-button variant="primary-link">Meu botão</fb-button>
-            <fb-button variant="secundary">Meu botão</fb-button>
-            <fb-button variant="secundary-link">Meu botão</fb-button>
-            <fb-button>
-                <div class="material-icons">settings</div>
-                Com ícones
-                <div class="material-icons">check</div>
-            </fb-button>
-            <fb-button isDisabled>Desabilitado</fb-button>
-            <fb-button isLoading>Carregando</fb-button>
-        </div>
+    <div class="component-examples flex flex-col gap-5">
+        <fb-card>
+            <fb-title-bar class="mb-4">
+                <template v-slot:left>Botões</template>
+            </fb-title-bar>
+            <fb-title-bar class="mb-4" title-type="subtitle">
+                <template v-slot:left>Variantes</template>
+            </fb-title-bar>
+            <div class="flex flex-wrap gap-2">
+                <fb-button>Meu botão</fb-button>
+                <fb-button variant="primary-outline">Meu botão</fb-button>
+                <fb-button variant="primary-link">Meu botão</fb-button>
+                <fb-button variant="secundary">Meu botão</fb-button>
+                <fb-button variant="secundary-link">Meu botão</fb-button>
+                <fb-button>
+                    <div class="material-icons">settings</div>
+                    Com ícones
+                    <div class="material-icons">check</div>
+                </fb-button>
+                <fb-button isDisabled>Desabilitado</fb-button>
+                <fb-button isLoading>Carregando</fb-button>
+            </div>
 
-        <fb-title-bar class="my-4" title-type="subtitle">
-            <template v-slot:left>Tamanhos</template>
-        </fb-title-bar>
-        <div class="flex gap-2">
-            <fb-button size="lg">Meu botão</fb-button>
-            <fb-button>Meu botão</fb-button>
-            <fb-button size="sm">Meu botão</fb-button>
-            <fb-button size="xs"> Meu botão </fb-button>
-        </div>
-        <fb-title-bar class="my-4" title-type="subtitle">
-            <template v-slot:left>Customizado com tailwind</template>
-        </fb-title-bar>
-        <fb-button size="lg" class="bg-purple-800 italic hover:bg-purple-600 w-60 rounded-sm" @click="() => console.log('Ola mundo')"
-            >Meu botão</fb-button
-        >
+            <fb-title-bar class="my-4" title-type="subtitle">
+                <template v-slot:left>Tamanhos</template>
+            </fb-title-bar>
+            <div class="flex flex-wrap gap-2">
+                <fb-button size="lg">Meu botão</fb-button>
+                <fb-button>Meu botão</fb-button>
+                <fb-button size="sm">Meu botão</fb-button>
+                <fb-button size="xs"> Meu botão </fb-button>
+            </div>
+            <fb-title-bar class="my-4" title-type="subtitle">
+                <template v-slot:left>Customizado com tailwind</template>
+            </fb-title-bar>
+            <fb-button size="lg" class="bg-purple-800 italic hover:bg-purple-600 w-60 rounded-sm" @click="() => console.log('Ola mundo')"
+                >Meu botão</fb-button
+            >
+        </fb-card>
+        <fb-card>
+            <fb-title-bar class="mb-4 mt-6">
+                <template v-slot:left>Tabela</template>
+            </fb-title-bar>
+            <fb-title-bar class="mb-4" title-type="subtitle">
+                <template v-slot:left>Simples</template>
+            </fb-title-bar>
+
+            <fb-table :items="self.tableData" :headers="self.headersTable"></fb-table>
+
+            <fb-title-bar class="mb-4" title-type="subtitle">
+                <template v-slot:left>Sem registros</template>
+            </fb-title-bar>
+            <fb-table :items="[]" :headers="self.headersTable"></fb-table>
+
+            <fb-title-bar class="my-4" title-type="subtitle">
+                <template v-slot:left>Customizando as colunas e cabeçalhos</template>
+            </fb-title-bar>
+
+            <fb-table :items="self.tableData" :headers="self.headersTable">
+                <template v-slot:header-price> <p class="font-bold text-red-800">Troquei o preço</p> </template>
+                <template v-slot:price="{ item }"> R$ {{ item.price }} </template>
+                <template v-slot:payment="{ item }">
+                    <p class="font-bold text-primary">{{ item.payment }}</p>
+                </template>
+                <template v-slot:status="{ item }">
+                    <div
+                        class="px-4 py-1 rounded-full text-white w-fit"
+                        :class="(self.dictStatus as any)[item.status]"
+                        size="small"
+                        rounded
+                    >
+                        {{ item.status }}
+                    </div>
+                </template>
+            </fb-table>
+        </fb-card>
     </div>
 </template>
 
 <script setup lang="ts">
 import FbTitleBar from '@/components/ui/fb-title-bar.vue';
 import FbButton from '@/components/ui/fb-button.vue';
+import FbTable from '@/components/ui/fb-table.vue';
+import { reactive } from 'vue';
+import FbCard from '@/components/ui/fb-card.vue';
+
+const self = reactive({
+    headersTable: [
+        { label: 'Nome', value: 'name' },
+        { label: 'Preço', value: 'price' },
+        { label: 'Pagamento', value: 'payment' },
+        { label: 'Status', value: 'status' },
+    ],
+    tableData: [
+        { name: 'Felipe', price: '1250,50', payment: 'Cartão', status: 'Pendente' },
+        { name: 'Davi', price: '830,50', payment: 'Debito', status: 'Pago' },
+        { name: 'Gustavo', price: '2,50', payment: 'Cartão', status: 'Pago' },
+        { name: 'Arthur', price: '35,00', payment: 'Dinheiro', status: 'Cancelado' },
+        { name: 'Jonas', price: '98,25', payment: 'PIX', status: 'Estornado' },
+        { name: 'Vilmar', price: '5531,96', payment: 'Vale', status: 'Pendente' },
+    ],
+    dictStatus: {
+        Pendente: 'bg-orange-500',
+        Pago: 'bg-green-500',
+        Cancelado: 'bg-red-500',
+        Estornado: 'bg-zinc-500',
+    },
+});
 </script>
 
 <style scoped></style>
