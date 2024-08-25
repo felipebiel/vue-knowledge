fe5f016aa67d320fc2555aecf7a23802cc651f9d
Felipe Biel <felipe.biel@t10.digital>
2024-08-25 14:14:56 -0300

    refactor: Limpando códigos

--- src/stories/components/ui/FbTable.stories.ts
+++ src/stories/components/ui/FbTable.stories.ts
@@ -1,7 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/vue3';
 import { fn } from '@storybook/test';
 import FbTable from '@/components/ui/fb-table.vue';
-import { reactive } from 'vue';
 
 const headersTable = [
     { label: 'Nome', value: 'name' },
@@ -23,29 +22,6 @@ const dictStatus = {
     Cancelado: 'bg-red-500',
     Estornado: 'bg-zinc-500',
 };
-
-const self = reactive({
-    headersTable: [
-        { label: 'Nome', value: 'name' },
-        { label: 'Preço', value: 'price' },
-        { label: 'Pagamento', value: 'payment' },
-        { label: 'Status', value: 'status' },
-    ],
-    tableData: [
-        { name: 'Felipe', price: '1250,50', payment: 'Cartão', status: 'Pendente' },
-        { name: 'Davi', price: '830,50', payment: 'Debito', status: 'Pago' },
-        { name: 'Gustavo', price: '2,50', payment: 'Cartão', status: 'Pago' },
-        { name: 'Arthur', price: '35,00', payment: 'Dinheiro', status: 'Cancelado' },
-        { name: 'Jonas', price: '98,25', payment: 'PIX', status: 'Estornado' },
-        { name: 'Vilmar', price: '5531,96', payment: 'Vale', status: 'Pendente' },
-    ],
-    dictStatus: {
-        Pendente: 'bg-orange-500',
-        Pago: 'bg-green-500',
-        Cancelado: 'bg-red-500',
-        Estornado: 'bg-zinc-500',
-    },
-});
 
 const meta = {
     title: 'UI/Table',
@@ -84,7 +60,7 @@ export const CustomSlots: Story = {
             return { args, self: { dictStatus: dictStatus } };
         },
         template: `
-        <fb-table v-bind="args" v-bind="self">
+        <fb-table v-bind="args">
             <template v-slot:header-price> <p class="font-bold text-red-800">Troquei o preço</p> </template>
             <template v-slot:price="{ item }"> R$ {{ item.price }} </template>
             <template v-slot:payment="{ item }">
