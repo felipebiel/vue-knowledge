52c566d6499674acf76dd3bafd3a5e7b93c11134
Felipe Biel <felipe.biel@t10.digital>
2024-08-27 15:19:39 -0300

    doc: Exemplos e Storybook

---
+++ src/stories/components/ui/FbModal.stories.ts
@@ -0,0 +1,180 @@
+import type { Meta, StoryObj } from '@storybook/vue3';
+
+import FbModal from '@/components/ui/fb-modal.vue';
+import { sizes, animationsModal } from '@/theme/constantes-theme';
+import { fn } from '@storybook/test';
+
+const meta = {
+    title: 'UI/Modal',
+    component: FbModal,
+    tags: ['autodocs'],
+    argTypes: {
+        size: {
+            options: [...sizes, 'full'],
+        },
+        animation: {
+            options: animationsModal,
+        },
+    },
+    args: {
+        onClose: fn(),
+        onConfirm: fn(),
+    },
+} satisfies Meta<typeof FbModal>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Simple: Story = {};
+
+export const Slots: Story = {
+    render: (args) => ({
+        components: { FbModal },
+        setup() {
+            return { args };
+        },
+        template: `
+        <fb-modal v-bind="args">
+            <template v-slot:header>Meu título</template>
+            <template v-slot:body>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vitae laoreet nunc. Ut non ex ultrices est accumsan facilisis vitae efficitur mauris. Mauris viverra augue erat, venenatis euismod mauris tincidunt egestas. Pellentesque interdum nec sapien et semper. Sed vel nunc euismod, volutpat lectus lobortis, finibus quam. Fusce placerat sapien sed felis sodales, at fermentum turpis pharetra. Nullam bibendum, dui eget rhoncus finibus, quam mi venenatis neque, sit amet tristique velit augue ut metus. Suspendisse nisl nulla, tempor vitae justo at, elementum interdum diam. Proin nec hendrerit nisl, eget auctor purus. Vestibulum gravida dapibus leo, a gravida nulla mollis vel. Mauris quis justo urna. Integer eget ullamcorper leo, eu pulvinar ante. Cras et nunc ut libero posuere vulputate non vitae mi. In imperdiet, massa at gravida mollis, nibh augue maximus arcu, vitae pretium nibh erat nec turpis. Duis condimentum ipsum eros, nec tempor magna hendrerit quis. Pellentesque euismod ullamcorper enim hendrerit scelerisque.
+                <p>
+            </template>
+        </fb-modal>`,
+    }),
+    decorators: [() => ({ template: '<div><story /></div>' })],
+};
+
+export const ExtaSmall: Story = {
+    args: {
+        size: 'xs',
+    },
+    render: (args) => ({
+        components: { FbModal },
+        setup() {
+            return { args };
+        },
+        template: `
+        <fb-modal v-bind="args">
+            <template v-slot:header>Meu título</template>
+            <template v-slot:body>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vitae laoreet nunc. Ut non ex ultrices est accumsan facilisis vitae efficitur mauris. Mauris viverra augue erat, venenatis euismod mauris tincidunt egestas. Pellentesque interdum nec sapien et semper. Sed vel nunc euismod, volutpat lectus lobortis, finibus quam. Fusce placerat sapien sed felis sodales, at fermentum turpis pharetra. Nullam bibendum, dui eget rhoncus finibus, quam mi venenatis neque, sit amet tristique velit augue ut metus. Suspendisse nisl nulla, tempor vitae justo at, elementum interdum diam. Proin nec hendrerit nisl, eget auctor purus. Vestibulum gravida dapibus leo, a gravida nulla mollis vel. Mauris quis justo urna. Integer eget ullamcorper leo, eu pulvinar ante. Cras et nunc ut libero posuere vulputate non vitae mi. In imperdiet, massa at gravida mollis, nibh augue maximus arcu, vitae pretium nibh erat nec turpis. Duis condimentum ipsum eros, nec tempor magna hendrerit quis. Pellentesque euismod ullamcorper enim hendrerit scelerisque.
+                <p>
+            </template>
+        </fb-modal>`,
+    }),
+    decorators: [() => ({ template: '<div><story /></div>' })],
+};
+
+export const Small: Story = {
+    args: {
+        size: 'sm',
+    },
+    render: (args) => ({
+        components: { FbModal },
+        setup() {
+            return { args };
+        },
+        template: `
+        <fb-modal v-bind="args">
+            <template v-slot:header>Meu título</template>
+            <template v-slot:body>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vitae laoreet nunc. Ut non ex ultrices est accumsan facilisis vitae efficitur mauris. Mauris viverra augue erat, venenatis euismod mauris tincidunt egestas. Pellentesque interdum nec sapien et semper. Sed vel nunc euismod, volutpat lectus lobortis, finibus quam. Fusce placerat sapien sed felis sodales, at fermentum turpis pharetra. Nullam bibendum, dui eget rhoncus finibus, quam mi venenatis neque, sit amet tristique velit augue ut metus. Suspendisse nisl nulla, tempor vitae justo at, elementum interdum diam. Proin nec hendrerit nisl, eget auctor purus. Vestibulum gravida dapibus leo, a gravida nulla mollis vel. Mauris quis justo urna. Integer eget ullamcorper leo, eu pulvinar ante. Cras et nunc ut libero posuere vulputate non vitae mi. In imperdiet, massa at gravida mollis, nibh augue maximus arcu, vitae pretium nibh erat nec turpis. Duis condimentum ipsum eros, nec tempor magna hendrerit quis. Pellentesque euismod ullamcorper enim hendrerit scelerisque.
+                <p>
+            </template>
+        </fb-modal>`,
+    }),
+    decorators: [() => ({ template: '<div><story /></div>' })],
+};
+
+export const Medium: Story = {
+    args: {
+        size: 'md',
+    },
+    render: (args) => ({
+        components: { FbModal },
+        setup() {
+            return { args };
+        },
+        template: `
+        <fb-modal v-bind="args">
+            <template v-slot:header>Meu título</template>
+            <template v-slot:body>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vitae laoreet nunc. Ut non ex ultrices est accumsan facilisis vitae efficitur mauris. Mauris viverra augue erat, venenatis euismod mauris tincidunt egestas. Pellentesque interdum nec sapien et semper. Sed vel nunc euismod, volutpat lectus lobortis, finibus quam. Fusce placerat sapien sed felis sodales, at fermentum turpis pharetra. Nullam bibendum, dui eget rhoncus finibus, quam mi venenatis neque, sit amet tristique velit augue ut metus. Suspendisse nisl nulla, tempor vitae justo at, elementum interdum diam. Proin nec hendrerit nisl, eget auctor purus. Vestibulum gravida dapibus leo, a gravida nulla mollis vel. Mauris quis justo urna. Integer eget ullamcorper leo, eu pulvinar ante. Cras et nunc ut libero posuere vulputate non vitae mi. In imperdiet, massa at gravida mollis, nibh augue maximus arcu, vitae pretium nibh erat nec turpis. Duis condimentum ipsum eros, nec tempor magna hendrerit quis. Pellentesque euismod ullamcorper enim hendrerit scelerisque.
+                <p>
+            </template>
+        </fb-modal>`,
+    }),
+    decorators: [() => ({ template: '<div><story /></div>' })],
+};
+
+export const Large: Story = {
+    args: {
+        size: 'lg',
+    },
+    render: (args) => ({
+        components: { FbModal },
+        setup() {
+            return { args };
+        },
+        template: `
+        <fb-modal v-bind="args">
+            <template v-slot:header>Meu título</template>
+            <template v-slot:body>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vitae laoreet nunc. Ut non ex ultrices est accumsan facilisis vitae efficitur mauris. Mauris viverra augue erat, venenatis euismod mauris tincidunt egestas. Pellentesque interdum nec sapien et semper. Sed vel nunc euismod, volutpat lectus lobortis, finibus quam. Fusce placerat sapien sed felis sodales, at fermentum turpis pharetra. Nullam bibendum, dui eget rhoncus finibus, quam mi venenatis neque, sit amet tristique velit augue ut metus. Suspendisse nisl nulla, tempor vitae justo at, elementum interdum diam. Proin nec hendrerit nisl, eget auctor purus. Vestibulum gravida dapibus leo, a gravida nulla mollis vel. Mauris quis justo urna. Integer eget ullamcorper leo, eu pulvinar ante. Cras et nunc ut libero posuere vulputate non vitae mi. In imperdiet, massa at gravida mollis, nibh augue maximus arcu, vitae pretium nibh erat nec turpis. Duis condimentum ipsum eros, nec tempor magna hendrerit quis. Pellentesque euismod ullamcorper enim hendrerit scelerisque.
+                <p>
+            </template>
+        </fb-modal>`,
+    }),
+    decorators: [() => ({ template: '<div><story /></div>' })],
+};
+
+export const Full: Story = {
+    args: {
+        size: 'full',
+    },
+    render: (args) => ({
+        components: { FbModal },
+        setup() {
+            return { args };
+        },
+        template: `
+        <fb-modal v-bind="args">
+            <template v-slot:header>Meu título</template>
+            <template v-slot:body>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vitae laoreet nunc. Ut non ex ultrices est accumsan facilisis vitae efficitur mauris. Mauris viverra augue erat, venenatis euismod mauris tincidunt egestas. Pellentesque interdum nec sapien et semper. Sed vel nunc euismod, volutpat lectus lobortis, finibus quam. Fusce placerat sapien sed felis sodales, at fermentum turpis pharetra. Nullam bibendum, dui eget rhoncus finibus, quam mi venenatis neque, sit amet tristique velit augue ut metus. Suspendisse nisl nulla, tempor vitae justo at, elementum interdum diam. Proin nec hendrerit nisl, eget auctor purus. Vestibulum gravida dapibus leo, a gravida nulla mollis vel. Mauris quis justo urna. Integer eget ullamcorper leo, eu pulvinar ante. Cras et nunc ut libero posuere vulputate non vitae mi. In imperdiet, massa at gravida mollis, nibh augue maximus arcu, vitae pretium nibh erat nec turpis. Duis condimentum ipsum eros, nec tempor magna hendrerit quis. Pellentesque euismod ullamcorper enim hendrerit scelerisque.
+                <p>
+            </template>
+        </fb-modal>`,
+    }),
+    decorators: [() => ({ template: '<div><story /></div>' })],
+};
+
+export const Animations: Story = {
+    args: {
+        speed: 1,
+        animation: 'newspaper',
+    },
+    render: (args) => ({
+        components: { FbModal },
+        setup() {
+            return { args };
+        },
+        template: `
+        <fb-modal v-bind="args">
+            <template v-slot:header>Meu título</template>
+            <template v-slot:body>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vitae laoreet nunc. Ut non ex ultrices est accumsan facilisis vitae efficitur mauris. Mauris viverra augue erat, venenatis euismod mauris tincidunt egestas. Pellentesque interdum nec sapien et semper. Sed vel nunc euismod, volutpat lectus lobortis, finibus quam. Fusce placerat sapien sed felis sodales, at fermentum turpis pharetra. Nullam bibendum, dui eget rhoncus finibus, quam mi venenatis neque, sit amet tristique velit augue ut metus. Suspendisse nisl nulla, tempor vitae justo at, elementum interdum diam. Proin nec hendrerit nisl, eget auctor purus. Vestibulum gravida dapibus leo, a gravida nulla mollis vel. Mauris quis justo urna. Integer eget ullamcorper leo, eu pulvinar ante. Cras et nunc ut libero posuere vulputate non vitae mi. In imperdiet, massa at gravida mollis, nibh augue maximus arcu, vitae pretium nibh erat nec turpis. Duis condimentum ipsum eros, nec tempor magna hendrerit quis. Pellentesque euismod ullamcorper enim hendrerit scelerisque.
+                <p>
+            </template>
+        </fb-modal>`,
+    }),
+    decorators: [() => ({ template: '<div><story /></div>' })],
+};
