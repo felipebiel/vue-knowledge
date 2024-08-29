509d8894bf212bd661a7700b1b078054516c6afa
Felipe Biel <felipe.biel@t10.digital>
2024-08-29 15:04:19 -0300

    feat: Regra de email

--- src/utils/validations/rules.ts
+++ src/utils/validations/rules.ts
@@ -5,6 +5,7 @@ export interface RulesInterface {
     customMessageRequired?: string;
     customMessageMinLength?: string;
     customMessageMaxLength?: string;
+    customMessageEmail?: string;
     minLength?: number;
     maxLength?: number;
 }
@@ -12,8 +13,8 @@ export interface RulesInterface {
 export const validateInput = (rule: RulesInterface | undefined | null, value: string | number) => {
     if (!rule) return '';
     switch (rule.type) {
-        // case 'email':
-        //     return emailValidate(rule, value);
+        case 'email':
+            return emailValidate(rule, value);
         case 'text':
             return textValidate(rule, value);
     }
@@ -23,22 +24,29 @@ export const validateInput = (rule: RulesInterface | undefined | null, value: st
 
 export const textValidate = (rule: RulesInterface, value: string | number): string => {
     const valueToString = new String(value);
-    if (rule.required && valueToString == '') {
+    const VALUE_LENGTH = valueToString.trim().split('').length;
+    if (rule.required && VALUE_LENGTH == 0) {
         return rule.customMessageRequired ? rule.customMessageRequired : `O campo ${rule.name} é obrigatório.`;
     }
-    if (
-        (valueToString == null || valueToString.trim().split('').length > 0) &&
-        rule.minLength &&
-        rule.minLength > valueToString.split('').length
-    ) {
+    if (VALUE_LENGTH > 0 && rule.minLength && rule.minLength > VALUE_LENGTH) {
         return rule.customMessageMinLength ? rule.customMessageMinLength : `Por favor, forneça ao menos ${rule.minLength} caracteres.`;
     }
-    if (
-        (valueToString == null || valueToString.trim().split('').length > 0) &&
-        rule.maxLength &&
-        rule.maxLength < valueToString.split('').length
-    ) {
+    if (VALUE_LENGTH > 0 && rule.maxLength && rule.maxLength < VALUE_LENGTH) {
         return rule.customMessageMaxLength ? rule.customMessageMaxLength : `Por favor, forneça até ${rule.maxLength} caracteres.`;
+    }
+
+    return '';
+};
+
+export const emailValidate = (rule: RulesInterface, value: string | number): string => {
+    const valueToString = new String(value);
+    const VALUE_LENGTH = valueToString.trim().split('').length;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (rule.required && VALUE_LENGTH == 0) {
+        return rule.customMessageRequired ? rule.customMessageRequired : `O campo ${rule.name} é obrigatório.`;
+    }
+    if (VALUE_LENGTH > 0 && !emailRegex.test(valueToString as string)) {
+        return rule.customMessageEmail ? rule.customMessageEmail : 'Insira um e-mail válido.';
     }
 
     return '';
